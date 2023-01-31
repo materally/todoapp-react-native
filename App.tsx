8706ae225b0e23b40be7b8a938d7983df3a3aeee
Kulcsar Mate <kulcsarmate@gmail.com>
2023-01-31 14:20:20 +0100

    initialize todo app

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+       
+        
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
