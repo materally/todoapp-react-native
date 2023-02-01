e1e8e6fd7a6533e5c061e21e4adc0f2c0c13a0a3
Kulcsar Mate <kulcsarmate@gmail.com>
2023-02-01 12:13:53 +0100

    feat: :sparkles: create routing logic

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { useRoutes } from './hooks/useRoutes';
+
+const Tab = createBottomTabNavigator();
 
 function App(): JSX.Element {
+  const routes = useRoutes();
+
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic">
-       
-        
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator>
+        {
+          routes.map(({ name, component, options }) => <Tab.Screen key={name} name={name} component={component} options={options} />)
+        }
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
