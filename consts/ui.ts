4d0e346ea8057786f07f41efb6cc51aeae86bba4
Kulcsar Mate <kulcsarmate@gmail.com>
2023-02-01 18:09:46 +0100

    fix: :pencil2: fix typo and layout

--- consts/ui.ts
+++ consts/ui.ts
@@ -2,7 +2,7 @@ export const text = {
   base: '#404040'
 }
 
-export const backgroud = {
+export const background = {
   base: '#F5F5F8',
 
   counter: '#D5D8E3'
--- screens/Completed.tsx
+++ screens/Completed.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import Header from '../components/Header';
 import Layout from '../components/Layout';
 
 export function CompletedScreen(): JSX.Element {
   return (
-    <Layout>
-      <Header title='Completed' count={0} />
-
+    <Layout title='Completed' count={0}>
       <View><Text>CompletedScreen</Text></View>
     </Layout>
   );
